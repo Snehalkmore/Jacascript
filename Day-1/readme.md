df30ae9431dcbbbd0794b6198880685accaba6e6
Snehal <snehalkmore@gmail.com>
2024-02-07 22:18:21 +0530

    day 1 code variable naming convensions

---
+++ Day-1/readme.md
@@ -0,0 +1,23 @@
+# Day 1 - Javascript 
+
+##  Variable Naming Convension
+
+below mathematical expression get executed 
+```
+console.log(40 + 8 + 23 - 7);
+```
+
+### Note 
+1. variable name should be in camel case (example - firstNameOfPerson) or "_" within each words like first_name_of_person.
+
+2. It cannot take reserved keywords as variables names like new or function keywords directly
+
+3. it can take only $ or _ in variable name like _function or $function
+
+4. "name" is keyword which is deprecated in javascript, so prefer not take it as variable name instead use firstName or fullName.
+
+5. Always start first letter of variable name with lower case.
+
+6. for constants like PI use all letter of variable name in upper case.
+
+7. variable names should be descriptive.
---
+++ Day-1/script.js
@@ -0,0 +1,35 @@
+let js = 'amazing';
+//if (js === 'amazing') alert('Javascript is fun!!!');
+//40 + 8 + 23 - 7;
+console.log(40 + 8 + 23 - 7);
+console.log("Snehal");
+console.log(4);
+
+let firstName = "Snehal";
+console.log(firstName);
+
+let name = 'snehal';
+console.log(name);
+
+let first_name = 'snehal';
+console.log(first_name);
+
+let PI = 3.1435;
+console.log(PI);
+
+let myFirstProgrammingLanguage = "Java";
+console.log(myFirstProgrammingLanguage);
+
+let myJob = "Accenture";
+console.log(myJob);
+
+//assignment -1
+console.log("Assignment -1");
+//Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+let country = "India";
+let continent = "Asia";
+let population = 1000000000;
+console.log(country);
+console.log(continent);
+console.log(population);
